dbda000db8d2d2e2b9261351fcd964bac58d6369
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2022-08-21 08:43:51 +0530

    Create 4.Multiple_Files.js

---
+++ 4.Multiple_Files.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-type','text/html');
+
+    let path = './Views';
+    switch(req.url){
+        case '/':
+            path += '/index.html';
+            break
+        case '/about':
+            path += '/About.html'
+            break;
+        default:
+            path += '/error.html';
+    }
+
+
+    fs.readFile(path,(err,fileData)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.end(fileData);
+        }
+    })
+})
+
+server.listen(3000,'localhost',()=>{
+    console.log('server is listening');
+})
